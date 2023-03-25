69e5d91e0a122b94cc8a820a47078dc407a671ff
serheihimself <sergm080@gmail.com>
2023-03-25 12:46:37 +0200

    final commit

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -44,7 +44,7 @@ export default function App() {
   };
 
   const loadMoreButton = async () => {
-    setPage(() => page + 1);
+    setPage(prev => prev + 1);
   };
 
   const showModal = imageId => {
@@ -68,73 +68,3 @@ export default function App() {
     </Container>
   );
 }
-
-// class App extends Component {
-//   state = {
-//     search: '',
-//     images: [],
-//     page: 1,
-//     pending: false,
-//     modal: '',
-//     error: '',
-//     totalImages: 0,
-//   };
-
-// async componentDidUpdate(_, prevState) {
-//   const { search, page } = this.state;
-//   if (search !== prevState.search || page !== prevState.page) {
-//     try {
-//       this.setState({ pending: true });
-//       const { images, totalImages } = await searchImage(search, page);
-//       if (images.length === 0) {
-//         this.setState({
-//           error: 'Were sorry, images is not found!',
-//         });
-//       }
-//       this.setState(prevState => ({
-//         images: [...prevState.images, ...images],
-//         error: '',
-//         totalImages,
-//       }));
-//     } catch (error) {
-//       this.setState({ error: 'Ooops..Something went wrong!' });
-//     } finally {
-//       this.setState({ pending: false });
-//     }
-//   }
-// }
-
-// searchNewImage = async inputValue => {
-//   this.setState({ search: inputValue, page: 1, images: [] });
-// };
-
-// loadMoreButton = async () => {
-//   this.setState(prev => ({ page: prev.page + 1 }));
-// };
-
-// showModal = imageId => {
-//   this.setState({ modal: imageId });
-// };
-
-// closeModal = () => {
-//   this.setState({ modal: '' });
-// };
-
-//   render() {
-//     const { images, pending, modal, totalImages, error } = this.state;
-// return (
-//   <Container>
-//     <Searchbar onSubmit={this.searchNewImage} />
-//     <ImageGallery images={images} showModal={this.showModal} />
-//     {images.length !== totalImages && !pending && (
-//       <Button loadMore={this.loadMoreButton} />
-//     )}
-//     {pending && <Loader />}
-//     {modal && <Modal image={modal} closeModal={this.closeModal} />}
-//     {error && <p>{error}</p>}
-//   </Container>
-// );
-//   }
-// }
-
-// export default App;
